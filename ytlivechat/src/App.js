335bb50e031195140d69798f3c575ae7f7517d0e
AlagusuryaPeriyakaruppan <alagup1405@gmail.com>
2024-09-16 00:16:30 +0530

    Initial commit - Live chat

---
+++ ytlivechat/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import VideoStream from "./components/live-chat/VideoStream";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoStream />
+    </div>
+  );
+}
+
+export default App;
